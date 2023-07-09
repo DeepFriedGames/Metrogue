6135580c7774c955c1a28aaaf9f0c2197bb8031d
deepfriedstu <stuart.fortier@gmail.com>
2023-07-09 09:01:20 -0600

    checking in

---
+++ desktop/bin/main/front_layer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="front_layer" tilewidth="16" tileheight="16" tilecount="680" columns="40">
+ <image source="_PNG/0. front layer.png" width="640" height="272"/>
+</tileset>
